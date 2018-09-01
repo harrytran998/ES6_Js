710eb289dc8ae71738582741fb184d4f3a4399fd
Loser Teach <jetaimefrc@gmail.com>
2018-09-01 15:01:47 +0700

    Update Module

---
+++ Module/Export.js
@@ -0,0 +1,5 @@
+export const deleStudent = (id) => {
+    console.log(`Student ${id} was deleted !`);
+}
+
+export const HelloBae = () => 'Motherfucker';
---
+++ Module/Import.js
@@ -0,0 +1,4 @@
+
+import * as lib from 'Export';
+// const a = HelloBae(XXX);
+console.log(lib.HelloBae());
